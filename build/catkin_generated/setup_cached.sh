902a0377558155b5d99214372d153f011ba079d1
miaofang <fangmiaomin@163.com>
2022-12-04 16:38:44 +0800

    first commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export ROSLISP_PACKAGE_DIRECTORIES='/home/miao/demo05_ws/devel/share/common-lisp'
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/miao/demo05_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/miao/demo05_ws/devel/lib:$LD_LIBRARY_PATH"
+export PWD='/home/miao/demo05_ws/build'
+export ROS_PACKAGE_PATH="/home/miao/demo05_ws/src:$ROS_PACKAGE_PATH"
